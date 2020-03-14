5bfed14d5e3786cc2648ef554b63d9d3c1ad1f1b
wybosys <wybosys@gmail.com>
2020-03-14 14:57:21 +0800

    prj

--- tests/parse_event_p.py
+++ tests/parse_event_p.py
@@ -8,7 +8,7 @@ add device 1: /dev/input/event7
   name:     "sdm660-snd-card-cdp Button Jack"
   events:
     KEY (0001): 0072  0073  00e2 
-        SW  (0005): 0002  0004  0006  0007  000f  0010  0012 
+    SW  (0005): 0002  0004  0006  0007  000f  0010  0012 
     ABS (0003): 0000  : value 0, min 0, max 1079, fuzz 0, flat 0, resolution 0
                 0001  : value 0, min 0, max 2279, fuzz 0, flat 0, resolution 0
                 0019  : value 0, min 0, max 255, fuzz 0, flat 0, resolution 0  
@@ -95,19 +95,49 @@ class EventInfo:
     TYPE_KEY = 1
     TYPE_ABS = 2
     TYPE_SW = 3
+
     def __init__(self):
         super().__init__()
         self.type = EventInfo.TYPE_UNKNOWN
-        self.type_value = 0
+        self.type_value = None
+
+    def read(self, d):
+        self.type_value = d[1]
+
+    @staticmethod
+    def Read(d):
+        #print(d)
+        typ = d[0]
+        if typ == 'KEY':
+          r = EventInfoKey()
+          if r.read(d):
+            return r
+        elif typ == 'ABS':
+          r = EventInfoAbs()
+          if r.read(d):
+            return r
+        return None
 
 class EventInfoKey(EventInfo):
     def __init__(self):
         super().__init__()
+        self.type = EventInfo.TYPE_KEY
         self.values = []
 
+    def read(self, d):
+        super().read(d)
+        self.values = d[2]
+        return True
+
+    def __repr__(self):      
+        print('KEY (%s):' % self.type_value)
+        print(self.values)
+        return ''
+        
 class EventInfoAbsValue:
     def __init__(self):
         super().__init__()
+        self.key = ''
         self.value = 0
         self.min = 0
         self.max = 0
@@ -116,23 +146,43 @@ class EventInfoAbsValue:
         self.resolution = 0
 
     def __repr__(self):
-        print('value %d, min %d, max %d, fuzz %d, flat %d, resolution $d' % (self.value, self.min, self.max, self.fuzz, self.flat, self.resolution))
+        print('key %s: value %d, min %d, max %d, fuzz %d, flat %d, resolution %d' % (self.key, self.value, self.min, self.max, self.fuzz, self.flat, self.resolution))
         return ''
+
+    def read(self, d):
+        #print(d)
+        self.key = d[0]
+        for e in d[1]:
+          k = e[0]
+          v = int(e[1])
+          if hasattr(self, k):
+            setattr(self, k, v)
+        return True
 
 class EventInfoAbs(EventInfo):
     def __init__(self):
         super().__init__()
-        self.value = ''
+        self.type = EventInfo.TYPE_ABS
         self.values = {}
 
     def __repr__(self):      
-        print('ABS (%s):' % self.value)
+        print('ABS (%s):' % self.type_value)
         print(self.values)
         return ''
+
+    def read(self, d):
+        #print(d)
+        super().read(d)
+        for e in d[2]:
+          r = EventInfoAbsValue()
+          if r.read(e):
+            self.values[r.key] = r
+        return True
 
 class EventInfoSw(EventInfo):
     def __init__(self):
         super().__init__()
+        self.type = EventInfo.TYPE_SW
         self.value = ''
         self.values = []
 
@@ -141,17 +191,10 @@ class EventInfoSw(EventInfo):
         print(self.values)
         return ''
 
-class EventInfoInputProps:    
-    INPUT_PROP_DIRECT = 1
-
 class EventDevice:
     def __init__(self):
         super().__init__()
-        self.index = 0
-        self.path = ''
-        self.name = ''
-        self.events = {}
-        self.input_props = None
+        self.clear()
 
     def __repr__(self):
         print('device %d: %s' % (self.index, self.path))
@@ -159,20 +202,51 @@ class EventDevice:
         print('events:')
         for k in self.events:
             print(self.events[k])
-        print('input props:')
-        print(self.input_props)        
         return ''
+
+    def clear(self):
+        self.index = 0
+        self.path = ''
+        self.name = ''
+        self.events = {}
+
+    def read(self, d):
+        self.index = int(d[0])
+        self.path = d[1]
+        self.name = d[2]
+        for r in d[3]:
+          e = EventInfo.Read(r)
+          if e:
+            self.events[e.type] = e
+        return True 
+
+class EventDevices:
+    def __init__(self):
+        super().__init__()
+        self.clear()
+
+    def clear(self):
+        self.devices = {}
 
     def parse(self, str):
         t = devices()        
         t.skipWhitespace = True                
         try:
           ret = t.parseString(str)
-          print(ret)
+          #print(ret)
+          # 转换list数据为结构数据          
+          for rd in ret:
+            d = EventDevice()
+            if d.read(rd):
+              self.devices[d.index] = d
         except:
           raise
           return False
         return True
+
+    def __repr__(self):
+        print(self.devices)
+        return ''
 
 # 构造ebnf解析器
 integer = pp.Word(pp.nums)
@@ -180,26 +254,27 @@ hex = pp.Word(pp.nums + 'abcdef')
 path = pp.Word(pp.alphanums + '/')
 key = pp.Word(pp.alphanums)
 
-info_name = 'name:' + pp.QuotedString('"')
+info_name = pp.Suppress('name:') + pp.QuotedString('"')
 info_value = pp.Suppress('(') + hex + pp.Suppress(')')
 
 info_event_key = 'KEY' + info_value + pp.Suppress(':') + pp.Group(pp.OneOrMore(hex))
 info_event_sw = 'SW' + info_value + pp.Suppress(':') + pp.OneOrMore(hex)
 info_event_abs_value = key + integer + pp.Suppress(pp.Optional(','))
 info_event_abs_values = hex + pp.Suppress(':') + pp.Group(pp.OneOrMore(pp.Group(info_event_abs_value)))
-info_event_abs = 'ABS' + info_value + pp.Suppress(':') + pp.OneOrMore(pp.Group(info_event_abs_values))
+info_event_abs = 'ABS' + info_value + pp.Suppress(':') + pp.Group(pp.OneOrMore(pp.Group(info_event_abs_values)))
 info_event = info_event_key | info_event_sw | info_event_abs
-info_events = pp.Suppress('events:') + pp.Group(pp.ZeroOrMore(pp.Group(info_event)))
+info_events = pp.Suppress('events:') + pp.Group(pp.OneOrMore(pp.Group(info_event)))
 
 input_prop_value = '<none>' | pp.Word(pp.string.ascii_uppercase + '_')
 input_props = 'input props:' + input_prop_value
 
-infos = pp.Suppress(info_name) + pp.Group(info_events) + pp.Group(input_props)
+infos = info_name + info_events + pp.Group(input_props)
 device = pp.Suppress('add device') + integer + pp.Suppress(':') + path + infos
 devices = pp.OneOrMore(pp.Group(device))
 
-ed = EventDevice()
+ed = EventDevices()
 if ed.parse(data):
     print(ed)
+    print('解析成功')
 else:
     print("解析失败")
