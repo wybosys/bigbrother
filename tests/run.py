8f9a2071aef797b6ef634fe5b172732b51965061
wybosys <wybosys@gmail.com>
2020-03-16 15:50:56 +0800

    prj

---
+++ tests/run.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3 
+
+import sys, subprocess, signal
+
+proc = subprocess.Popen(' '.join(sys.argv[1:]), shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+def doquit(signum=0, frame=0):
+    proc.terminate()
+
+signal.signal(signal.SIGINT, doquit)
+signal.signal(signal.SIGTERM, doquit)
+
+while True:
+    line = proc.stdout.readline()
+    line = line.decode("utf-8", "ignore")
+    if proc.poll() != None:        
+        break
+    print(line, end='')
+
+ret = proc.communicate()[1]
+if ret:
+    exit(ret)
